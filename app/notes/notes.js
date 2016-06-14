aea82955eee011b498533ea1775cd285215f2acb
mreed19 <michael.reed315@gmail.com>
2016-06-14 13:45:14 -0400

    Wrap meganote.notes module in an IIFE

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -1,41 +1,43 @@
-angular.module('meganote.notes', [
-  'ui.router'
-])
+(function() {
+  angular.module('meganote.notes', ['ui.router'])
+  .config(notesConfig)
+  .controller('NotesController', NotesController);
 
-.config(function($stateProvider) {
-  $stateProvider
+  function notesConfig($stateProvider) {
+    $stateProvider
 
-    .state('notes', {
-      url: '/notes',
-      templateUrl: 'notes/notes.html',
-      controller: 'NotesController'
-    })
+      .state('notes', {
+        url: '/notes',
+        templateUrl: 'notes/notes.html',
+        controller: 'NotesController'
+      })
 
-    .state('notes.form', {
-      url: '/:noteId',
-      templateUrl: 'notes/notes-form.html'
-    });
-})
-
-.controller('NotesController', function($scope) {
-  $scope.editing = false;
-  $scope.notes = [];
-  $scope.note = {};
-  $scope.addNote = function() {
-    $scope.notes.push($scope.note);
-    $scope.note = {};
-    $scope.note.title = '';
-    $scope.note.body = '';
+      .state('notes.form', {
+        url: '/:noteId',
+        templateUrl: 'notes/notes-form.html'
+      });
   }
-  $scope.removeNote = function(index) {
-    $scope.notes.splice(index, 1);
-  }
-  $scope.editNote = function(note) {
-    $scope.editing = true;
-    $scope.note = note;
-  }
-  $scope.updateNote = function() {
-    $scope.note = {};
+
+  function NotesController($scope) {
     $scope.editing = false;
+    $scope.notes = [];
+    $scope.note = { title: '', body: ''};
+    $scope.addNote = function() {
+      $scope.notes.push($scope.note);
+      $scope.note = {};
+      $scope.note.title = '';
+      $scope.note.body = '';
+    }
+    $scope.removeNote = function(index) {
+      $scope.notes.splice(index, 1);
+    }
+    $scope.editNote = function(note) {
+      $scope.editing = true;
+      $scope.note = note;
+    }
+    $scope.updateNote = function() {
+      $scope.note = {};
+      $scope.editing = false;
+    }
   }
-});
+})();
