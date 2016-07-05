861223f9d8982eab8e297e81be3a6987793e7466
mreed19 <michael.reed315@gmail.com>
2016-07-05 15:43:50 -0400

    Update app module to conform to style guide

---
+++ app/app.module.js
@@ -0,0 +1,15 @@
+(function() {
+  angular.module('meganote', [
+    'ui.router',
+    'ngFlash',
+    'textAngular',
+    'meganote.notes'
+  ])
+  .config(configFunction);
+
+  configFunction.$inject = ['$urlRouterProvider'];
+  function configFunction($urlRouterProvider) {
+    $urlRouterProvider.otherwise('/notes/');
+  }
+
+})();
