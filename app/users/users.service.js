e2412c9c945a81ea02a7440ef730b58c8571ddb0
mreed19 <michael.reed315@gmail.com>
2016-07-07 15:08:33 -0400

    Refactor sign-up directive to use UsersService

--- app/users/users.service.js
+++ app/users/users.service.js
@@ -1,12 +1,21 @@
 {
-  class UsersService {
-    create(user) {
-      console.log('CREATED!');
-      console.log(user);
-    }
-  }
-
   angular
     .module('meganote.users')
-    .service('UsersService', UsersService);
+    .service('UsersService', [
+      '$http',
+      'API_BASE',
+      ($http, API_BASE) =>{
+
+        class UsersService {
+          create(user) {
+            return $http.get(API_BASE)
+              .then(res => {
+                console.log(res.data);
+              });
+          }
+        }
+
+        return new UsersService();
+      }
+    ]);
 }
