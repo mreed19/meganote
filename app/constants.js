b7bd30aace6527847186d91bb0a84c5106dd263e
mreed19 <michael.reed315@gmail.com>
2016-07-15 13:35:20 -0400

    Move static files to root directory. Change API_BASE to heroku server

--- app/constants.js
+++
@@ -1,5 +0,0 @@
-{
-  angular
-    .module('meganote')
-    .constant('API_BASE', 'http://localhost:3030/api/v1/');
-}
---
+++ constants.js
@@ -0,0 +1,5 @@
+{
+  angular
+    .module('meganote')
+    .constant('API_BASE', 'https://mreed19-meganote-server.herokuapp.com/api/v1/');
+}
